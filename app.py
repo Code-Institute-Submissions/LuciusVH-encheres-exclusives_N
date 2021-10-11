22c80c9ecb6f21b6ab34ec691cd22e47815eff83
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2021-10-11 15:32:22 +0200

    implemented flash messages

--- app.py
+++ app.py
@@ -74,6 +74,27 @@ def register():
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
+  if request.method == "POST":
+    # Check if the email is already in the database
+    existing_user = mongo.db.users.find_one(
+      {"email": request.form.get("email").lower()}
+    )
+
+    if existing_user:
+      # Check if the hashed password matches the user input
+      if check_password_hash(
+        existing_user["password"], request.form.get("password")):
+          session["user"] = request.form.get("email").lower()
+          flash("Welcome, {}".format(request.form.get("email")))
+          return redirect(url_for("profile", email=session["user"]))
+      else:
+        # Invalid password
+        flash("Incorrect email and/or password.")
+        return redirect(url_for("login"))
+    else:
+      # Email not registered
+      flash("Incorrect email and/or password.")
+      return redirect(url_for("login"))
   return render_template('login.html')
 
 
