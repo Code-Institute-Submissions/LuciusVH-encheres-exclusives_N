26c2c3526dc835effe3ffd573834cc614a7bca44
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2021-10-12 14:35:00 +0200

    display password instructions when registering

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -28,3 +28,18 @@ function changeBgImg() {
     header.className = "hero-section bg-dark py-5 hero-img-landscape"
   }
 }
+
+// Show the password instructions when the user selects the password field when registering
+const passportField = document.querySelector('.password-register');
+const passportInstructions = document.querySelector('.password-instructions');
+
+passportField.addEventListener('focus', showInstructions);
+passportField.addEventListener('blur', hideInstructions);
+
+function showInstructions() {
+  passportInstructions.style.display = 'block';
+}
+
+function hideInstructions() {
+  passportInstructions.style.display = 'none';
+}
