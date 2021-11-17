b495aa28622325796ae727fa2bed30dccf477448
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2021-11-17 16:42:39 +0100

    display user's bids on profile

--- static/js/glide.js
+++ static/js/glide.js
@@ -18,4 +18,11 @@ const config = {
   }
 };
 
-new Glide('.glide', config).mount();
+// Enable multiple sliders on the same page
+// Code snippet found on https://github.com/glidejs/glide/issues/59#issuecomment-529124814 
+const sliders = document.querySelectorAll('.glide');
+
+Object.values(sliders).map(slideshow => {
+  const slider = new Glide(slideshow, config);
+  slider.mount();
+});
